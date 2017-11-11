5801da4b09e0c1bce786decc80e1d85e7895e819
sjiaming <450109441@qq.com>
2017-11-11 17:41:41 +0800

    moved head.js to about.html

--- assets/js/pdfFromHTML.js
+++ assets/js/pdfFromHTML.js
@@ -8,24 +8,21 @@ specialElementHandlers = {
 }
 margins = {
     top: 50,
-    bottom: 50,
     left: 60,
-    right: 60
+    width: 545
   };
-
 pdf.fromHTML(
   	source // HTML string or DOM elem ref.
   	, margins.left // x coord
   	, margins.top // y coord
-    , margins.bottom
-    , margins.right
   	, {
-  		 'elementHandlers': specialElementHandlers
+  		'width': margins.width // max width of content on PDF
+  		, 'elementHandlers': specialElementHandlers
   	},
   	function (dispose) {
   	  // dispose: object with X, Y of the last line add to the PDF
   	  //          this allow the insertion of new lines after html
-        pdf.save('title.pdf');
+        pdf.save('html2pdf.pdf');
       }
   )		
 }
